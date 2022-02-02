1dc76edf93a1c686360a85450546aeba3a6023e7
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:00:30 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,17 +1,17 @@
 #lang racket
 
-;; number-elements: List -> List(List(Int, Value))
-;; usage: (number-elements lst) = mark each element of list as a pair of its index and value
-(define number-elements
+;; list.number-elements: List -> List(List(Int, Value))
+;; usage: (list.number-elements lst) = mark each element of list as a pair of its index and value
+(define list.number-elements
   (lambda (lst)
-    (number-elements-from 0 lst)))
+    (list.number-elements-from 0 lst)))
 
-(define number-elements-from
+(define list.number-elements-from
   (lambda (n lst)
     (if (null? lst)
         '()
         (cons (list n (car lst))
-              (number-elements-from (+ n 1) (cdr lst))))))
+              (list.number-elements-from (+ n 1) (cdr lst))))))
 
 ; s-list ::= ({s-exp}*)
 ; s-exp  ::= symbol | s-list
